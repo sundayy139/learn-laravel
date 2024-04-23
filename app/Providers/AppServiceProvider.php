d98041c079895a675c2b688227fd594609a49e50
sundayy139 <voduytam139@gmail.com>
2024-04-23 21:19:17 +0700

    Limit rating đối với user đã đăng nhập

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('api', function(Request $request){
+            if($request -> method('GET')) {
+                return Limit::perSecond(60) -> by(auth() -> id());
+            }
+
+            return Limit::perSecond(10) -> by(auth() -> id());
+        });
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware -> api('throttle:api');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
